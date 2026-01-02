97d04e64225361ee3467b4383d6affbe0a2f443f
glentechkenya <glentech287@gmail.com>
2026-01-02 11:45:47 +0300

    Update crick.js

--- plugins/crick.js
+++ plugins/crick.js
@@ -28,7 +28,7 @@ const text = m.body.slice(prefix.length + cmd.length).trim();
       const result = response.data;
 
       let formattedResult = `╭══════════════•∞•══╮\n`;
-      formattedResult += `│⿻   *RAHEEM-XMD-2🔥*\n`;
+      formattedResult += `│⿻   *GLEN-X*\n`;
       formattedResult += `│⿻   *LIVE MATCH INFO* ✨\n`;
       formattedResult += `│⿻\n`;
 
