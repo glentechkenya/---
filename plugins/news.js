48aac542b36bc01358cab5969965084969c56649
glentechkenya <glentech287@gmail.com>
2026-01-02 12:26:33 +0300

    Update news.js

--- plugins/news.js
+++ plugins/news.js
@@ -24,7 +24,7 @@ async (conn, mek, m, { from, reply }) => {
 ⚠️ _${article.description}_
 🔗 _${article.url}_
 
-  ©⎈ RAHEEM-XMD-2🪀 ⎈
+  ©⎈ GLEN-X ⎈
             `;
 
             console.log('Article URL:', article.urlToImage); // Log image URL for debugging
